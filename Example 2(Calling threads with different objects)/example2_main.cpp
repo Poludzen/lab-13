1d0421fd829791e64d5e723da2528a06e8f3e61e
Poludzen <80580379+Poludzen@users.noreply.github.com>
2022-02-21 11:22:05 +0300

    Update example2_main.cpp

--- Example 2(Calling threads with different objects)/example2_main.cpp
+++ Example 2(Calling threads with different objects)/example2_main.cpp
@@ -1,14 +1,8 @@
 // CPP program to demonstrate multithreading
-
 // using three different callables.
-
 #include <iostream>
-
 #include <thread>
-
 using namespace std;
-  
-
 // A dummy function
 
 void foo(int Z){
@@ -17,7 +11,6 @@ void foo(int Z){
                " pointer as callable\n";
     }
 }  
-
 // A callable object
 class thread_obj {
 public:
@@ -33,45 +26,26 @@ int main(){
          "operating independently" << endl;
   
     // This thread is launched by using 
-
     // function pointer as callable
-
     thread th1(foo, 3);
-
     // This thread is launched by using
-
     // function object as callable
     thread th2(thread_obj(), 3);  
-
     // Define a Lambda Expression
-
     auto f = [](int x) {
         for (int i = 0; i < x; i++)
             cout << "Thread using lambda"
              " expression as callable\n";
     }; 
-
     // This thread is launched by using 
-
     // lamda expression as callable
-
     thread th3(f, 3);
-
     // Wait for the threads to finish
-
     // Wait for thread t1 to finish
     th1.join();
-
     // Wait for thread t2 to finish
-
     th2.join();
-
-  
-
     // Wait for thread t3 to finish
-
     th3.join();
-
     return 0;
-
 }
